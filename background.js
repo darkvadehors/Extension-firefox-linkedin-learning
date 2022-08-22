e6b83faf7a7b8991a169b34050e055e5e27aa347
JohnBen <darkvadehors@users.noreply.github.com>
2022-08-22 17:10:05 +0200

    reoutr dasn la background OK

--- background.js
+++ background.js
@@ -4,6 +4,7 @@
 
 function onExecuted(result) {
 	console.log(`We executed in all subframes`);
+	return;
 }
 
 function onError(error) {
@@ -12,74 +13,72 @@ function onError(error) {
 
 (async () => {
 	// receive the array from script.js
-	browser.runtime.onMessage.addListener((request) => {
+	browser.runtime.onMessage.addListener((requestCs) => {
 		// console.log('request', request);
 		// console.log('typeofrequest', typeof request);
 
-		if (request.courses_url) {
+		if (requestCs.courses_url) {
 			// get the lenght of the array courses_url
-			let length = request.courses_url.length;
+			let length = requestCs.courses_url.length;
 			console.log("nombre d'onglet a ouvrir ", length);
 
-            // color in red the number on Icon
-            browser.browserAction.setBadgeBackgroundColor({
-                color: 'red',
-            });
+			// color in red the number on Icon
+			browser.browserAction.setBadgeBackgroundColor({
+				color: 'red',
+			});
+
+			console.log('requestCs.courses_url', requestCs.courses_url);
+
 			// create a new tab for each course
 			// For ...of version
-			for (const element of request.courses_url) {
+			for (const element of requestCs.courses_url) {
 				// add the number of the course and the red color to the button
 				browser.browserAction.setBadgeText({ text: length.toString() });
 
 				// open each tabs
-				browser.tabs.create({ url: element }, (tab) => {
-					console.log('tab', tab.id);
-					chrome.tabs.onUpdated.addListener(async (tabId, tab) => {
-						console.log('avant await', tabId);
+				(async () => {
+					browser.tabs.create({ url: element }, (tab) => {
+						console.log('tab', tab.id);
 
-						var executingScript = await chrome.tabs
-							.executeScript(tabId, { file: 'tabs.js' })
+						console.log('avant await');
+
+						browser.tabs
+							.executeScript(tab.id, { file: 'tabs.js' })
 							.then((results) => {
 								// The content script's last expression will be true if the function
 								// has been defined. If this is not the case, then we need to run
 								// clipboard-helper.js to define function copyToClipboard.
 								if (!results || results[0] !== true) {
-									return browser.tabs.executeScript(tab.id, {
-										file: 'clipboard-helper.js',
-									});
+									console.log("result ", results);
 								}
-							})
-							.then(() => {
-								return results;
 							})
 							.catch((error) => {
 								// This could happen if the extension is not allowed to run code in
 								// the page, for example if the tab is a privileged page.
-								console.error('Failed to get url: ' + error);
-							});
+								console.error('Failed to copy text: ' + error);
+							}); // execute the script in the new tab with tab.id
 
-						executingScript.then(onExecuted, onError);
 						console.log('after await');
 					});
-				});
-
+				})();
+				/**
+				 * Fonction Future
+				 */
 				// on ferme la tab precedement ouverte
-				browser.tabs.remove(tab.id);
-
-				// on telecharge la video grace a l'url récupéré
-				browser.downloads.download({
-					url: request.video_url,
-					filename: 'video.mp4',
-				});
+				// browser.tabs.remove(tab.id);
 
 				/**
-				 *  En attente de trouver une solution pour trouver la balise avec l'url de la video
-				 * chrome.tabs.executeScript(tab.id, { file: 'download.js' });
+				 * Fonction Future
 				 */
+				// on telecharge la video grace a l'url récupéré
+				// browser.downloads.download({
+				// 	url: request.video_url,
+				// 	filename: 'video.mp4',
+				// });
+				length--;
 			}
 
 			// decrement de 1 the length
-			length--;
 		}
 	});
 })();
--- tabs.js
+++ tabs.js
@@ -5,21 +5,26 @@
 	console.log('Entre dans tabs.js');
 
     // retarder l'execution de la fonction
-	setTimeout(() => {
-		// get the url of the video course
-		const videoObject = document.querySelectorAll('video.vjs-tech');
 
-		// declare videoData Object
-		const videoData = {
-			videoTitle: videoObject[0].ownerDocument.title,
-			videoTastModified: videoObject[0].ownerDocument.lastModified,
-			videoUrl: videoObject[0].currentSrc,
-		};
+        const result = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                // get the url of the video course
+                const videoObject = document.querySelectorAll('video.vjs-tech');
 
-		console.log('videoUrl currentSrc', videoData);
+                // declare videoData Object
+                const videoData = {
+                    videoTitle: videoObject[0].ownerDocument.title,
+                    videoTastModified: videoObject[0].ownerDocument.lastModified,
+                    videoUrl: videoObject[0].currentSrc,
+                };
 
-		// send array to background.js
-		browser.runtime.sendMessage({ url: videoData });
-		return videoUrl;
-	}, 1000);
+                console.log(videoData);
+
+                // send array to background.js
+                resolve(videoData);
+            }, 1000);
+        }
+        );
+        return result;
+
 })();

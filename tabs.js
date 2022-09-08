e36f9ccb0561386611403d957c3f76e4eecd0ff2
JohnBen <darkvadehors@users.noreply.github.com>
2022-09-08 18:21:33 +0200

    random timer & formationTitle

--- tabs.js
+++ tabs.js
@@ -10,12 +10,16 @@
 			console.log('Entre dans tabs.setTimeout');
 			// get the url of the video course
 			const videoObject = document.querySelectorAll('video.vjs-tech');
+            const formationTitle = document.querySelectorAll("div.classroom-nav__details > h1")
+
+            console.log('formationTitle :>> ', formationTitle[0].innerText);
 			console.log('videoObject', videoObject);
 			console.log('videoObject currentSrc', videoObject[0].currentSrc);
 			// console.log('videoObject src', videoObject[0].src);
 
 			// declare videoData Object
 			videoData = {
+                formationTilte : formationTitle[0].innerText,
 				videoTitle: videoObject[0].ownerDocument.title,
 				videoTastModified: videoObject[0].ownerDocument.lastModified,
 				videoUrl: videoObject[0].src,
@@ -25,7 +29,7 @@
 
 			// send array to background.js
 			resolve(1);
-		}, 2000);
+		}, Math.floor(Math.random() * (7000 - 2000) + 2000)); // change the setTimeout  whith random value
 	});
 
 	return videoData;

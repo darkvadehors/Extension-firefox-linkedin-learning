9e445772a1d1c49774bd0af12a1587e9ac0ab9be
JohnBen <darkvadehors@users.noreply.github.com>
2022-09-08 19:38:02 +0200

    clean

--- tabs.js
+++ tabs.js
@@ -1,37 +1,38 @@
 /** @format */
 'use strict';
 (async () => {
-	console.log('Entre dans tabs.js');
 
-    let timer = Math.floor(Math.random() * (7000 - 2000) + 2000);
-    let minuter = parseInt(timer/1000)
+	let timer = Math.floor(Math.random() * (7000 - 2000) + 2000);
+	let minuter = parseInt(timer / 1000);
 
-    setInterval(() => {
-        document.title = "Closed in " + minuter ;
-        minuter --
-    }, 1000);
-    var videoData;
+	// countdown before closing
+	setInterval(async () => {
+		document.title = 'Close in ' + minuter + ' sec.';
+		minuter--;
+	}, 1000);
+	var videoData;
 
-	await new Promise((resolve, reject) => {
-        document.title = "Ne pas toucher !";
+	await new Promise(async (resolve) => {
+		// document title
+		document.title = 'Do not touch !';
 		setTimeout(() => {
-
 			// get the url of the video course
 			const videoObject = document.querySelectorAll('video.vjs-tech');
-            const formationTitle = document.querySelectorAll("div.classroom-nav__details > h1")
+			const formationTitle = document.querySelectorAll(
+				'div.classroom-nav__details > h1',
+			);
 
 			// declare videoData Object
 			videoData = {
-                formationTilte : formationTitle[0].innerText,
+				formationTilte: formationTitle[0].innerText,
 				videoTitle: videoObject[0].ownerDocument.title,
 				videoTastModified: videoObject[0].ownerDocument.lastModified,
 				videoUrl: videoObject[0].src,
 			};
 
-
 			// send array to background.js
 			resolve(1);
-		}, timer ); // change the setTimeout  whith random value
+		}, timer); // change the setTimeout  whith random value
 	});
 
 	return videoData;

0fd37d796c0ebe03e5d578c28338c1750567cc16
JohnBen <darkvadehors@users.noreply.github.com>
2022-09-08 16:59:40 +0200

    chargement async =>OK

--- tabs.js
+++ tabs.js
@@ -1,30 +1,32 @@
 /** @format */
 'use strict';
-(async() => {
-
+(async () => {
 	console.log('Entre dans tabs.js');
 
-    // retarder l'execution de la fonction
+    var videoData;
 
-        const result = await new Promise((resolve, reject) => {
-            setTimeout(() => {
-                // get the url of the video course
-                const videoObject = document.querySelectorAll('video.vjs-tech');
+	await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Entre dans tabs.setTimeout');
+			// get the url of the video course
+			const videoObject = document.querySelectorAll('video.vjs-tech');
+			console.log('videoObject', videoObject);
+			console.log('videoObject currentSrc', videoObject[0].currentSrc);
+			// console.log('videoObject src', videoObject[0].src);
 
-                // declare videoData Object
-                const videoData = {
-                    videoTitle: videoObject[0].ownerDocument.title,
-                    videoTastModified: videoObject[0].ownerDocument.lastModified,
-                    videoUrl: videoObject[0].currentSrc,
-                };
+			// declare videoData Object
+			videoData = {
+				videoTitle: videoObject[0].ownerDocument.title,
+				videoTastModified: videoObject[0].ownerDocument.lastModified,
+				videoUrl: videoObject[0].src,
+			};
 
-                console.log(videoData);
+			console.log(videoData);
 
-                // send array to background.js
-                resolve(videoData);
-            }, 2000);
-        }
-        );
-        return await result;
+			// send array to background.js
+			resolve(1);
+		}, 2000);
+	});
 
+	return videoData;
 })();
